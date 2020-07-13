c1135857d0a844ed0fdb8955eff5470639cab197
Myke <mykemitrichev@gmail.com>
2020-07-13 04:35:25 +0300

    Created models, handled form submission

--- pizza/views.py
+++ pizza/views.py
@@ -5,5 +5,12 @@ def home(request):
 
 
 def order(request):
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            cleaned_data = filled_form.cleaned_data
+            note = 'Thanks for ordering. Your {} {} and {} pizza is on its way!'.format(cleaned_data['size'],cleaned_data['topping1'],cleaned_data['topping2'])
+            new_form = PizzaForm()    
+            return render(request, 'pizza/order.html',{'pizzaform':new_form, 'note':note})
     pizzaForm = PizzaForm()
     return render(request, 'pizza/order.html', {'pizzaform' : pizzaForm})
